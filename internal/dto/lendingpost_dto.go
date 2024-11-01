0c3704b9f97dab6283d6e5ff67b477516bf6f0d4
bpremika <premika.yu@gmail.com>
2024-11-01 19:41:36 +0700

    fix: add json

--- internal/dto/lendingpost_dto.go
+++ internal/dto/lendingpost_dto.go
@@ -3,19 +3,19 @@ package dto
 import "time"
 
 type CreateLendingPostRequest struct {
-	ItemName    string
-	Description string
-	Price       float64
-	ImageURL    string
+	ItemName    string  `json:"item_name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	ImageURL    string  `json:"image_url"`
 }
 
 type LendingPost struct {
-	Id          uint64
-	ItemName    string
-	Description string
-	Price       float64
-	ImageURL    string
-	OwnerId     uint64
-	OwnerName   string
-	CreatedAt   time.Time
+	Id          uint64    `json:"id"`
+	ItemName    string    `json:"item_name"`
+	Description string    `json:"description"`
+	Price       float64   `json:"price"`
+	ImageURL    string    `json:"image_url"`
+	OwnerId     uint64    `json:"owner_id"`
+	OwnerName   string    `json:"owner_name"`
+	CreatedAt   time.Time `json:"created_at"`
 }
